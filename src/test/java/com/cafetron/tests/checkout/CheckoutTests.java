a50e7a80e263427989f3215bc18c97d6ba658495
kiran kalluru <129094970+kirankalluru@users.noreply.github.com>
2026-06-30 10:07:25 +0530

    feat: Enhance checkout tests by adding CartPage interaction and validating checkout route

--- src/test/java/com/cafetron/tests/checkout/CheckoutTests.java
+++ src/test/java/com/cafetron/tests/checkout/CheckoutTests.java
@@ -2,6 +2,7 @@ package com.cafetron.tests.checkout;
 
 import com.cafetron.base.AuthenticatedBaseTest;
 import com.cafetron.flows.CartCheckoutFlow;
+import com.cafetron.pages.CartPage;
 import com.cafetron.pages.CheckoutPage;
 import org.testng.Assert;
 import org.testng.SkipException;
@@ -18,10 +19,12 @@ public class CheckoutTests extends AuthenticatedBaseTest {
             throw skip;
         }
 
+        CartPage cartPage = new CartPage(getDriver());
+        cartPage.clickCheckout();
+
         CheckoutPage checkoutPage = new CheckoutPage(getDriver());
-        checkoutPage.open();
+        Assert.assertTrue(checkoutPage.waitForUrlContains("/checkout"), "Checkout route should open from cart");
         Assert.assertTrue(checkoutPage.isDisplayed(), "Checkout UI should be visible");
-        Assert.assertTrue(checkoutPage.hasTotals(), "Checkout should show totals");
     }
 
     @Test(description = "TC-042 to TC-044: Verify checkout validation feedback is visible",
